23ff62eb3ce7576c08af38a41e956359577973a4
nabeel2pc <mohdnabeel2pc@gmail.com>
2025-09-21 12:14:38 +0530

    Fix configs for Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mess-app-starter/',  // 👈 important for GitHub Pages
+  base: '/', // ✅ root path for Vercel
+  build: {
+    outDir: 'dist',
+  },
 })
